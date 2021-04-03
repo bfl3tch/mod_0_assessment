4d1a91b9dc6b6b2435679529a1acfeca65d97849
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-04-02 20:47:33 -0400

    fix missing comma syntax

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,12 +1,12 @@
 fake_dinosaur = {
-  species_name => "AidenAva"
-  main_color => "blue"
-  height_in_feet => 4
-  daily_temper_tantrums => 32
-  tall => false
-  has_unlimited_energy => true
-  breakfast => [ "waffles", "bacon" ]
-  dinner => [ "meat", "vegetables" ]
+  species_name => "AidenAva",
+  main_color => "blue",
+  height_in_feet => 4,
+  daily_temper_tantrums => 32,
+  tall => false,
+  has_unlimited_energy => true,
+  breakfast => [ "waffles", "bacon" ],
+  dinner => [ "meat", "vegetables" ],
   dessert => [ "whatever just stop asking", "ice cream" ]
 
 }
--- models/model_b.rb
+++ models/model_b.rb
@@ -1,12 +1,12 @@
 fake_developer = {
-  name => "Co Cacola"
-  position => "entry_level"
-  salary => 85,034.78
-  months_in_position => 6
-  is_productive => true
-  good_for_you => false
-  options_instead => ["water", "green tea"]
+  name => "Co Cacola",
+  position => "entry_level",
+  salary => 85,034.78,
+  months_in_position => 6,
+  is_productive => true,
+  good_for_you => false,
+  options_instead => ["water", "green tea"],
   size_choices_in_ounces => [ 12, 20, 64 ]
-  
+
 
 }
