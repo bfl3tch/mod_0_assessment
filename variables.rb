0e6f4cbd01b61d0b9f7a35bb7364013bc4129e54
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-04-02 20:24:07 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Out Cold"
+is_entertaining = true
+movie = "Surf Ninjas"
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+favorite_foods = ["pizza", "sushi", "steak"]
+favorite_foods.shift
+movie.size
