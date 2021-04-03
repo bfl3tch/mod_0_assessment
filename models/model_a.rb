9cf3ceb898aa4ddc5df727d0e80a5ba5d618ce25
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-04-02 20:39:14 -0400

    Fix syntax

--- models/model_a.rb
+++ models/model_a.rb
@@ -3,10 +3,10 @@ fake_dinosaur = {
   main_color => "blue"
   height_in_feet => 4
   daily_temper_tantrums => 32
-  tall = false
-  has_unlimited_energy = true
-  breakfast = [ "waffles", "bacon" ]
-  dinner = [ "meat", "vegetables" ]
-  dessert = [ "whatever just stop asking", "ice cream" ]
+  tall => false
+  has_unlimited_energy => true
+  breakfast => [ "waffles", "bacon" ]
+  dinner => [ "meat", "vegetables" ]
+  dessert => [ "whatever just stop asking", "ice cream" ]
 
 }
